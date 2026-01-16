c279246d97e27f2dfee631aa9e865fb05d90e032
Soham Ghadai <sohamghadai28@gmail.com>
2026-01-16 23:31:52 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+async function weatherlao(city){
+    try{
+    let apikey = '19bb5f2cf083fafef2eefe1136e1c70e';
+    let raw = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`);
+    if(!raw.ok){
+        throw new Error("city not found or something went wrong");
+    }
+    let real = await raw.json();
+    if(real.main.temp < 0){
+        console.warn(`TOO COLD OUT THERE.....${real.main.temp}C`)
+    }else if(real.main.temp > 0){
+        console.warn(`TOO HOT OUT THERE....${real.main.temp}C`)
+    }
+    }catch(err){
+        console.log(err.message);
+    }
+};
+weatherlao("Noida");
